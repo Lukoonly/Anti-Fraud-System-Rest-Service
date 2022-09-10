a6c1d46f4440a74d52171053ddf78408061102a9
admin <admin@MacBook-Pro-admin-2.local>
2022-09-10 12:30:02 +0300

    added project

---
+++ src/main/java/antifraud/api/dto/StolenCardDTO.java
@@ -0,0 +1,15 @@
+package antifraud.api.dto;
+
+import lombok.Getter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Getter
+public class StolenCardDTO {
+    @NotNull
+    @NotEmpty
+    @NotBlank
+    String number;
+}
